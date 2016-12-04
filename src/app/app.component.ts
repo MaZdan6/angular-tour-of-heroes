8f8c95910319d9053bb2d5d9509570cd87ec6f08
Mateusz Zdanowicz <fazol66@gmail.com>
2016-12-04 23:05:56 +0100

    3. Master/Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+//Dekorator
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',// uri to szablonu html
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  heroes= HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void{
+  this.selectedHero = hero;
+  }
 }
+
+// deklaracja klasy
+export class Hero{
+  id: number;
+  name: string;
+}
+
+//lista obiektów typu Hero
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice'},
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
