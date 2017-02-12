2ffc173a4706838c5e6d717c6f448b40c15f018f
Mateusz Zdanowicz <fazol66@gmail.com>
2017-02-12 22:03:20 +0100

    5.Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,34 @@
 import { Component } from '@angular/core';
 import{Hero} from './hero';
+import{HeroService} from './hero.service';
+import{OnInit} from '@angular/core';
+
 //Dekorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',// uri to szablonu html
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HeroService]//wstrzykiwanie zależności
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes= HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
+ // kontruktor tworzy instancję 'heroService' (wstrzykiwanie zależności)
+  constructor(private heroService: HeroService){}
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes=> this.heroes= heroes);
+  }
+  
   onSelect(hero: Hero): void{
   this.selectedHero = hero;
+  }
+
+  ngOnInit(): void{
+    this.getHeroes();
   }
 }
 
 
-//lista obiektów typu Hero
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice'},
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
